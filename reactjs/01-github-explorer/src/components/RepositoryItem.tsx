5257cc6bf171a9cba7032ca6abb67ea554da291d
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2021-03-17 23:42:55 -0300

    Fundamentos do ReactJS

---
+++ reactjs/01-github-explorer/src/components/RepositoryItem.tsx
@@ -0,0 +1,20 @@
+interface IRepositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+export function RepositoryItem({ repository }: IRepositoryItemProps) {
+  return (
+    <li>
+      <strong>{repository.name}</strong>
+      <p>{repository.description}</p>
+
+      <a href={repository.html_url} target="_blank">
+        Acessar repositório
+      </a>
+    </li>
+  )
+}
---
+++ reactjs/01-github-explorer/src/components/RepositoryList.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+import { RepositoryItem } from "./RepositoryItem";
+
+import '../styles/repositories.scss';
+
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
+export function RepositoryList() {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+      .then(response => response.json())
+      .then(setRepositories)
+  }, []);
+
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositórios</h1>
+
+      <ul>
+        {repositories.map((repository, key) => (
+          <RepositoryItem key={key} repository={repository} />
+        ))}
+      </ul>
+    </section>
+  )
+}
