b5c213c74819122bf55fdf9c0eb75afef5d1fb42
Lucas Eduardo <lucas.eduardo.developer@gmail.com>
2021-03-22 14:51:39 -0300

    iniciando a api

---
+++ nodejs/rentalx/src/modules/cars/repositories/implementations/CategoriesRepository.ts
@@ -0,0 +1,45 @@
+import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
+
+import { Category } from "../../model/Category";
+
+class CategoriesRepository implements ICategoriesRepository {
+  private categories: Category[];
+
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
+    this.categories = [];
+  }
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
+  }
+
+  create({ name, description }: ICreateCategoryDTO): void {
+    const category = new Category();
+
+    Object.assign(category, {
+      name,
+      description,
+      create_at: new Date()
+    });
+
+    this.categories.push(category);
+  }
+
+  list(): Category[] {
+    return this.categories;
+  }
+
+  findByName(name: string): Category {
+    const category = this.categories.find((category) => category.name === name);
+
+    return category;
+  }
+}
+
+export { CategoriesRepository };
---
+++ nodejs/rentalx/src/routes/specifications.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+
+import { createSpecificationController } from '../modules/cars/useCases/createSpecification';
+
+
+const specificationsRoutes = Router();
+
+specificationsRoutes.post('/', (req, res) => createSpecificationController.handle(req, res));
+
+export { specificationsRoutes };
